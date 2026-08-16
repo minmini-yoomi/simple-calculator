eef4683239c7e53e494ed391126e185906a8e3bc
minmini-yoomi <bangtanminyoongi793@gmail.com>
2026-08-16 13:20:13 +0300

    initial commit : simple calculator project, wrapped the program in a function so I can call it later in a separated file, and some other details

--- simple_calculator.py
+++ simple_calculator.py
@@ -11,39 +11,41 @@ def to_float(num):
     except ValueError:
         print("ERROR: You must enter a number.")
         return None
-exit_flag = False
-while True :
-    num1 = None
-    num2 = None
-    res = None
-    while num1==None:
-        num1 = input("number 1: ")
-        if not check_x(num1):
-            exit_flag = True
-            break
-        num1 = to_float(num1)
-    if exit_flag : break
-    operate = input("the operation (+ , - , * , /): ")
-    if not check_x(operate): break
-    while num2 == None:
-        num2 = input("number 2: ")
-        if not check_x(num2):
-            exit_flag= True
-            break
-        num2 = to_float(num2)
-    if exit_flag : break
 
-    if operate == "+":
-        res = num1 + num2
-    elif operate == "-":
-        res = num1 - num2
-    elif operate == "*":
-        res = num1 * num2
-    elif operate == "/":
-        if num2 != 0:
-            res = num1 / num2
+def calculator():
+    while True:
+        num1 = None
+        num2 = None
+        res = None
+        exit_flag = False
+        while num1 is None:
+            num1 = input("number 1: ")
+            if not check_x(num1):
+                exit_flag = True
+                break
+            num1 = to_float(num1)
+        if exit_flag: break
+        operate = input("the operation (+ , - , * , /): ")
+        if not check_x(operate): break
+        while num2 is None:
+            num2 = input("number 2: ")
+            if not check_x(num2):
+                exit_flag = True
+                break
+            num2 = to_float(num2)
+        if exit_flag: break
+
+        if operate == "+":
+            res = num1 + num2
+        elif operate == "-":
+            res = num1 - num2
+        elif operate == "*":
+            res = num1 * num2
+        elif operate == "/":
+            if num2 != 0:
+                res = num1 / num2
+            else:
+                print("ERROR: Division by zero is not allowed")
         else:
-            print("ERROR: Division by zero is not allowed")
-    else:
-        print("INVALID INPUT")
-    print("the result is: ", res)
+            print("INVALID INPUT")
+        print("the result is: ", res)
